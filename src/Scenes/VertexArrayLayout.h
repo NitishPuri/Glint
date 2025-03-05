7e63757abb96798e2e9bd9f24324451406fd8161
Nitish Puri <npuri1903@gmail.com>
2025-03-05 14:54:05 +0530

    vao+

---
+++ src/Scenes/VertexArrayLayout.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Renderer.h"
+
+// TODO: Implement this class to handle vertex layout automatically
+class VertexArrayLayout {
+ public:
+  void addAttribute(GLuint location, GLint size, GLenum type, GLboolean normalized = GL_FALSE) {
+    // Handle attribute layout automatically
+  }
+};
