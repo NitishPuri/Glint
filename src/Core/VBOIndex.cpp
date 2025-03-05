e0e16e6b3831e038d3f5138d0045858ab588ebb4
Nitish Puri <npuri1903@gmail.com>
2025-03-05 14:28:13 +0530

    warn+

--- src/Core/VBOIndex.cpp
+++ src/Core/VBOIndex.cpp
@@ -32,7 +32,7 @@ bool getSimilarVertexIndex(glm::vec3& in_vertex, glm::vec2& in_uv, glm::vec3& in
 void indexVBO_slow(std::vector<glm::vec3>& in_vertices, std::vector<glm::vec2>& in_uvs,
                    std::vector<glm::vec3>& in_normals,
                    // outputs
-                   std::vector<unsigned short>& out_indices, std::vector<glm::vec3>& out_vertices,
+                   std::vector<index_t>& out_indices, std::vector<glm::vec3>& out_vertices,
                    std::vector<glm::vec2>& out_uvs, std::vector<glm::vec3>& out_normals) {
   // For each input vertex
   for (unsigned int i = 0; i < in_vertices.size(); i++) {
