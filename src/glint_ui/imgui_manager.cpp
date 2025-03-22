c0f17298a027f61f633e2ee6da9af5e0859aba7c
Nitish Puri <npuri1903@gmail.com>
2025-03-22 22:02:12 +0530

    Refactor descriptor pool creation; reduce pool sizes and add TODO for future enhancements. Update CMakeLists to set runtime output directory for samples.

--- src/glint_ui/imgui_manager.cpp
+++ src/glint_ui/imgui_manager.cpp
@@ -82,22 +82,29 @@ void ImGuiManager::cleanup() {
 }
 
 void ImGuiManager::createDescriptorPool() {
-  VkDescriptorPoolSize pool_sizes[] = {{VK_DESCRIPTOR_TYPE_SAMPLER, 1000},
-                                       {VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1000},
-                                       {VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE, 1000},
-                                       {VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, 1000},
-                                       {VK_DESCRIPTOR_TYPE_UNIFORM_TEXEL_BUFFER, 1000},
-                                       {VK_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER, 1000},
-                                       {VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 1000},
-                                       {VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1000},
-                                       {VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC, 1000},
-                                       {VK_DESCRIPTOR_TYPE_STORAGE_BUFFER_DYNAMIC, 1000},
-                                       {VK_DESCRIPTOR_TYPE_INPUT_ATTACHMENT, 1000}};
+  // VkDescriptorPoolSize pool_sizes[] = {{VK_DESCRIPTOR_TYPE_SAMPLER, 1000},
+  //                                      {VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1000},
+  //                                      {VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE, 1000},
+  //                                      {VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, 1000},
+  //                                      {VK_DESCRIPTOR_TYPE_UNIFORM_TEXEL_BUFFER, 1000},
+  //                                      {VK_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER, 1000},
+  //                                      {VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 1000},
+  //                                      {VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1000},
+  //                                      {VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC, 1000},
+  //                                      {VK_DESCRIPTOR_TYPE_STORAGE_BUFFER_DYNAMIC, 1000},
+  //                                      {VK_DESCRIPTOR_TYPE_INPUT_ATTACHMENT, 1000}};
+
+  // TODO: Add more descriptor types if needed, or reduce the count
+  VkDescriptorPoolSize pool_sizes[] = {
+      {VK_DESCRIPTOR_TYPE_SAMPLER, 100},
+      {VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 100},  // For images/textures
+      {VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 100},          // For uniform data
+  };
 
   VkDescriptorPoolCreateInfo pool_info = {};
   pool_info.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
   pool_info.flags = VK_DESCRIPTOR_POOL_CREATE_FREE_DESCRIPTOR_SET_BIT;
-  pool_info.maxSets = 1000;
+  pool_info.maxSets = 100;
   pool_info.poolSizeCount = std::size(pool_sizes);
   pool_info.pPoolSizes = pool_sizes;
 
